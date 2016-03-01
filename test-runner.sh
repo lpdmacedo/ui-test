43650f4f467b662f623babd46141df4d170a34c9
Leonardo De Macedo <leonardo.macedo@mercadolibre.com.ar>
2016-03-01 15:03:38 -0300

    Refactor script to run functional test

--- test-runner.sh
+++ test-runner.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
+#source /commands/load_grails.sh /app/application.properties
 
 apt-get --assume-yes  install libfontconfig1
 
 #Initialize variables to default values.
 SCRIPT_NAME=TEST-RUNNER
-UI_TEST_FOLDER=/ui-test
+UI_TEST_FOLDER=/app/ui-test
 APP_FOLDER=/app
 OPT_ENV=local
 OPT_SITE=mla
@@ -15,67 +16,20 @@ ENV_LOCAL_REMOTE=local_remote
 ENV_JENKINS=jenkins
 ENV_JENKINS_REMOTE=jenkins_remote
 
-#Set fonts for Help.
-NORM=`tput sgr0`
-BOLD=`tput bold`
-REV=`tput smso`
-
-cd $UI_TEST_FOLDER
-
 #Help function
-function HELP {
-  echo  \\n"Help documentation for ${BOLD}${SCRIPT_NAME}.${NORM}"\\n
+help() {
+  echo "Help documentation for ${SCRIPT_NAME}."
   echo "Command line switches are optional. The following switches are recognized."
-  echo "${REV}-e${NORM}  --Sets the value for option ${BOLD}env${NORM}. Default is ${BOLD}$OPT_ENV [$ENV_LOCAL_REMOTE - $ENV_JENKINS - $ENV_JENKINS_REMOTE]${NORM}."
-  echo "${REV}-s${NORM}  --Sets the value for option ${BOLD}site${NORM}. Default is ${BOLD}$OPT_SITE${NORM}."
-  echo "${REV}-b${NORM}  --Sets the value for option ${BOLD}browser${NORM}. Default is ${BOLD}$OPT_BROWSER${NORM}."
-  echo "${REV}-h${NORM}  --Displays this help message."\\n
-  echo "Example: ${BOLD}$SCRIPT -e local -b chrome -s mla${NORM}"\\n
+  echo "-e  --Sets the value for option env. Default is $OPT_ENV [$ENV_LOCAL_REMOTE - $ENV_JENKINS - $ENV_JENKINS_REMOTE]."
+  echo "-s  --Sets the value for option site. Default is $OPT_SITE."
+  echo "-b  --Sets the value for option browser. Default is $OPT_BROWSER."
+  echo "-h  --Displays this help message."
+  echo "Example: SCRIPT_NAME -e local -b chrome -s mla"
   exit 1
 }
 
-#Check the number of arguments. If none are passed, print help and exit.
-NUMARGS=$#
-if [ $NUMARGS -eq 0 ]; then
-  HELP
-fi
-
-### Start getopts code ###
-
-#Parse command line flags
-while getopts :e:s:b:h FLAG; do
-  case $FLAG in
-    e)  #set option "a"
-      OPT_ENV=$OPTARG
-      echo "-e used: $OPTARG"
-      export UI_TEST_ENV=$OPTARG
-      ;;
-    s)  #set option "b"
-      OPT_SITE=$OPTARG
-      echo "-s used: $OPTARG"
-      export UI_TEST_SITE=$OPTARG
-      ;;
-    b)  #set option "c"
-      OPT_BROWSER=$OPTARG
-      echo "-b used: $OPTARG"
-      export UI_TEST_BROWSER=$OPTARG
-      ;;
-    h)  #show help
-      HELP
-      ;;
-    \?) #unrecognized option - show help
-      echo \\n"Option -${BOLD}$OPTARG${NORM} not allowed."
-      HELP
-      ;;
-  esac
-done
-
-shift $((OPTIND-1))
-
-### End getopts code ###
-
 ###... Wait To Start App ...###
-function waitForPing() {
+waitForPing() {
   COUNT=0
   echo "Waiting for app up"
   while [[ $COUNT -lt 1200 ]]; do
@@ -91,83 +45,107 @@ function waitForPing() {
   exit 100
 }
 
-###... Wait To Start Selenium Server ...###
-function waitForSeleniumServer() {
-	OUNT=0
-  	echo "Waiting for start selenium-server"
-  	while [[ $COUNT -lt 1200 ]]; do
-    	response=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:4444/wd/hub/status)
-    	if [[ "$OUT" == "200" ]]; then
-      		echo "Selenium started"
-      		return
-    	fi
-    	sleep 1;
-    	COUNT=$(( $COUNT + 1 ))
-  	done
-  echo "Wait for /wd/hub/status timedout"
-  exit 100
+###... Start Selenium Server ...###
+startSeleniumServer() {
+  COUNT=0
+    cd $UI_TEST_FOLDER
+    java -jar ./node_modules/selenium-standalone/.selenium/selenium-server/2.50.1-server.jar -browser "browserName=$OPT_BROWSER,platform=LINUX" &
+    echo "Waiting for start selenium-server"
+    while [ $COUNT -lt 1200 ]; do
+      OUT=$(curl --write-out %{http_code} --silent --output /dev/null http://127.0.0.1:4444/wd/hub/status)
+      if [ "$OUT" -eq "200" ]; then
+          echo "Selenium started"
+          return
+      fi
+      sleep 1;
+      COUNT=$(( $COUNT + 1 ))
+    done
+    echo "Wait for /wd/hub/status timedout"
+    exit 100
+}
+
+#... Install functional test dependencies ...#
+installDependencies() {
+    cd $UI_TEST_FOLDER
+    npm cache clean
+  npm install
+  npm install -g nightwatch
+  ./node_modules/selenium-standalone/bin/selenium-standalone install
 }
 
 ###... Run Local App ...###
-function run_application() {
-	grails $GRAILS_OPTS run-app &
+runApplication() {
+    echo "------- Starting grails app -------"
+    echo "-------" $GRAILS_OPTS
+    cd $APP_FOLDER
+  grails $GRAILS_OPTS run-app --non-interactive &
 }
 
 
 ###... Test Execution ...###
-function executeTest() {
-	cd $APP_FOLDER
-	grails GRAILS_OPTS test-app --non-interactive
+executeTest() {
+    echo "------- Test Execution -------"
+    installDependencies
+  startSeleniumServer
+  runApplication
 
-	if [[ $OPT_ENV -eq local ]]; then
-		run_application
-		waitForPing
-	fi
+  cd $UI_TEST_FOLDER
+  nightwatch -e $OPT_BROWSER
 
-	cd $UI_TEST_FOLDER
+  status=$?
+    echo "Terminating jobs..."
+    jobs -p | xargs kill
+  running=$(jobs | wc -l)
+  count=0
+  while [ $running -gt 0 ]; do
+    if [ $count -gt 30 ]; then
+       break
+    fi
+    sleep 1
+    count=$(( $count + 1 ))
+    running=$(jobs | wc -l)
+  done
 
-	if [ -d $PATH_NODE_MODULES ]; then
-		nightwatch -e $OPT_BROWSER
-  	else
-  		npm run e2e
-		nightwatch -e $OPT_BROWSER
-	fi
-
-	status=$?
-  	echo "Terminating jobs..."
-  	jobs -p | xargs kill
-	  running=$(jobs | wc -l)
-	  count=0
-	  while [ $running -gt 0 ]; do
-	    if [ $count -gt 30 ]; then
-	        break
-	    fi
-	    sleep 1
-	    count=$(( $count + 1 ))
-	    running=$(jobs | wc -l)
-	  done
-	  if [ $running -gt 0 ]; then
-	      echo "Killing jobs..."
-	      jobs -p | xargs kill -9
-	  fi
-	  exit $status
-	
-	#Customize script to run selenium server manually
-	#npm install
-	#npm install -g nightwatch
-	#./node_modules/selenium-standalone/bin/selenium-standalone install
-	#java -jar ./node_modules/selenium-standalone/.selenium/selenium-server/2.50.1-server.jar -browser "browserName=$OPT_BROWSER,platform=LINUX" &
-	#waitForSeleniumServer
-	#npm run e2e
-	#nightwatch -e $OPT_BROWSER
+  if [ $running -gt 0 ]; then
+    echo "Killing jobs..."
+    jobs -p | xargs kill -9
+  fi
+  exit $status
 }
 
-if [ $NUMARGS -gt 0 ]; then
-  	executeTest
-else
-	echo "Build is being generated, don't run tests..."
-	exit 0
-fi
+#Parse command line flags
+while getopts :e:s:b:h FLAG; do
+  case $FLAG in
+    e)  #set option "env"
+      OPT_ENV=$OPTARG
+      echo "-e used: $OPTARG"
+      export UI_TEST_ENV=$OPTARG
+      ;;
+    s)  #set option "site"
+      OPT_SITE=$OPTARG
+      echo "-s used: $OPTARG"
+      export UI_TEST_SITE=$OPTARG
+      ;;
+    b)  #set option "browser"
+      OPT_BROWSER=$OPTARG
+      echo "-b used: $OPTARG"
+      export UI_TEST_BROWSER=$OPTARG
+      ;;
+    h)  #show help
+      help
+      ;;
+    \?) #unrecognized option - show help
+      echo \\n"Option -$OPTARG not allowed."
+      help
+      ;;
+  esac
+done
 
+shift $((OPTIND-1))
+
+### End getopts code ###
+executeTest
+echo "Build is being generated, don't run tests..."
+exit 0
 
 
