58573b7cb0bdc115e3bef2cbd97a7bb63656a29e
Leonardo De Macedo <leonardo.macedo@mercadolibre.com.ar>
2016-03-01 15:26:59 -0300

    Format code & configure grails run app

--- test-runner.sh
+++ test-runner.sh
@@ -29,7 +29,7 @@ help() {
 }
 
 ###... Wait To Start App ...###
-waitForPing() {
+waitForStartApp() {
   COUNT=0
   echo "Waiting for app up"
   while [[ $COUNT -lt 1200 ]]; do
@@ -48,26 +48,26 @@ waitForPing() {
 ###... Start Selenium Server ...###
 startSeleniumServer() {
   COUNT=0
-    cd $UI_TEST_FOLDER
-    java -jar ./node_modules/selenium-standalone/.selenium/selenium-server/2.50.1-server.jar -browser "browserName=$OPT_BROWSER,platform=LINUX" &
-    echo "Waiting for start selenium-server"
-    while [ $COUNT -lt 1200 ]; do
-      OUT=$(curl --write-out %{http_code} --silent --output /dev/null http://127.0.0.1:4444/wd/hub/status)
-      if [ "$OUT" -eq "200" ]; then
-          echo "Selenium started"
-          return
-      fi
-      sleep 1;
-      COUNT=$(( $COUNT + 1 ))
-    done
-    echo "Wait for /wd/hub/status timedout"
-    exit 100
+  cd $UI_TEST_FOLDER
+  java -jar ./node_modules/selenium-standalone/.selenium/selenium-server/2.50.1-server.jar -browser "browserName=$OPT_BROWSER,platform=LINUX" &
+  echo "Waiting for start selenium-server"
+  while [ $COUNT -lt 1200 ]; do
+    OUT=$(curl --write-out %{http_code} --silent --output /dev/null http://127.0.0.1:4444/wd/hub/status)
+    if [ "$OUT" -eq "200" ]; then
+      echo "Selenium started"
+      return
+    fi
+    sleep 1;
+    COUNT=$(( $COUNT + 1 ))
+  done
+  echo "Wait for /wd/hub/status timedout"
+  exit 100
 }
 
 #... Install functional test dependencies ...#
 installDependencies() {
-    cd $UI_TEST_FOLDER
-    npm cache clean
+  cd $UI_TEST_FOLDER
+  npm cache clean
   npm install
   npm install -g nightwatch
   ./node_modules/selenium-standalone/bin/selenium-standalone install
@@ -75,70 +75,72 @@ installDependencies() {
 
 ###... Run Local App ...###
 runApplication() {
-    echo "------- Starting grails app -------"
-    echo "-------" $GRAILS_OPTS
-    cd $APP_FOLDER
+  echo "------- Starting grails app -------"
+  echo "-------" $GRAILS_OPTS
+  cd $APP_FOLDER
   grails $GRAILS_OPTS run-app --non-interactive &
 }
 
 
 ###... Test Execution ...###
 executeTest() {
-    echo "------- Test Execution -------"
-    installDependencies
+  echo "------- Test Execution -------"
+  installDependencies
   startSeleniumServer
   runApplication
+  waitForStartApp
+
 
   cd $UI_TEST_FOLDER
   nightwatch -e $OPT_BROWSER
 
   status=$?
-    echo "Terminating jobs..."
-    jobs -p | xargs kill
+  echo "Terminating jobs..."
+  jobs -p | xargs kill
   running=$(jobs | wc -l)
   count=0
   while [ $running -gt 0 ]; do
     if [ $count -gt 30 ]; then
-       break
-    fi
-    sleep 1
-    count=$(( $count + 1 ))
-    running=$(jobs | wc -l)
-  done
+     break
+   fi
+   sleep 1
+   count=$(( $count + 1 ))
+   running=$(jobs | wc -l)
+ done
 
-  if [ $running -gt 0 ]; then
-    echo "Killing jobs..."
-    jobs -p | xargs kill -9
-  fi
-  exit $status
+ if [ $running -gt 0 ]; then
+  echo "Killing jobs..."
+  jobs -p | xargs kill -9
+fi
+exit $status
 }
 
 #Parse command line flags
 while getopts :e:s:b:h FLAG; do
   case $FLAG in
     e)  #set option "env"
-      OPT_ENV=$OPTARG
-      echo "-e used: $OPTARG"
-      export UI_TEST_ENV=$OPTARG
-      ;;
+OPT_ENV=$OPTARG
+echo "-e used: $OPTARG"
+export UI_TEST_ENV=$OPTARG
+;;
     s)  #set option "site"
-      OPT_SITE=$OPTARG
-      echo "-s used: $OPTARG"
-      export UI_TEST_SITE=$OPTARG
-      ;;
+OPT_SITE=$OPTARG
+echo "-s used: $OPTARG"
+export UI_TEST_SITE=$OPTARG
+;;
     b)  #set option "browser"
-      OPT_BROWSER=$OPTARG
-      echo "-b used: $OPTARG"
-      export UI_TEST_BROWSER=$OPTARG
-      ;;
+OPT_BROWSER=$OPTARG
+echo "-b used: $OPTARG"
+export UI_TEST_BROWSER=$OPTARG
+;;
     h)  #show help
-      help
-      ;;
+help
+;;
     \?) #unrecognized option - show help
-      echo \\n"Option -$OPTARG not allowed."
-      help
-      ;;
-  esac
+echo \\n"Option -$OPTARG not allowed."
+help
+;;
+esac
 done
 
 shift $((OPTIND-1))
